e5d67306d813dadcbcabe5960342d58e556f973d
Gaurav <goyal1092.gmailcom>
2015-02-01 22:52:09 +0530

    WIP fro user login via flask login

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask
 from flask.ext.pymongo import PyMongo
 from flask.ext.mail import Mail
-
+from flask.ext.login import LoginManager
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,4 +13,13 @@ mail = Mail(app)
 app.config['MONGO_DBNAME'] = 'TrainingPortal'
 mongo = PyMongo(app, config_prefix='MONGO')
 
-from app import views
+login = LoginManager()
+login.init_app(app)
+
+@login.user_loader
+def load_user(user_id):
+    return mongo.db.User.find_one_or_404({'_id':ObjectId(user_id)})
+
+
+from app import views, models
+from models import User
